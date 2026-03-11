d2d5161a329993bb66224d5c52e7583e943cec9c
Aderson <iamuchoa@gmail.com>
2026-03-10 23:37:34 -0300

    feat: add seedworks

---
+++ Domain/Interfaces/SeedWorks/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Interfaces.SeedWorks
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitAsync(CancellationToken cancellationToken = default);
+        DbContext GetEfDbContext();
+        Task RollbackAsync(CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Infrastructure/SeedWorks/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces.SeedWorks;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SeedWorks
+{
+    public sealed class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly TransactManagerContext _entityFrameworkContext;
+        private bool _isDisposed;
+
+        public UnitOfWork(TransactManagerContext entityFrameworkContext)
+        {
+            _entityFrameworkContext = entityFrameworkContext;
+            _isDisposed = false;
+        }
+
+        public async Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            await _entityFrameworkContext.SaveChangesAsync(cancellationToken);
+            await _entityFrameworkContext.Database.CommitTransactionAsync(cancellationToken);
+        }
+
+        private void Disposing(bool disposing)
+        {
+            if (_isDisposed && disposing)
+                _entityFrameworkContext.Dispose();
+
+            _isDisposed = true;
+        }
+
+        public async Task RollbackAsync(CancellationToken cancellationToken = default)
+        {
+            await _entityFrameworkContext.Database.RollbackTransactionAsync(cancellationToken);
+        }
+
+        public Task BeginTransactionAsync(CancellationToken cancellationToken = default) =>
+            _entityFrameworkContext.Database.BeginTransactionAsync(cancellationToken);
+
+        public DbContext GetEfDbContext() => _entityFrameworkContext;
+
+        public void Dispose() => Disposing(true);
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default) =>
+            _entityFrameworkContext.SaveChangesAsync(cancellationToken);
+    }
+}
