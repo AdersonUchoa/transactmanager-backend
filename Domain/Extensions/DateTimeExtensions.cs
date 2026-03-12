5f592b7d024e3c6c1ec6e692f54ff07f2b799d7f
Aderson <iamuchoa@gmail.com>
2026-03-11 21:41:57 -0300

    feat: new extensions

---
+++ Domain/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToStringDate(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd");
+        }
+    }
+}
---
+++ Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Domain.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string Value(this Enum e)
+        {
+            var enumMember =
+                e.GetType()
+                 .GetMember(e.ToString())[0]
+                 .GetCustomAttribute<EnumMemberAttribute>()?
+                 .Value;
+
+            if (string.IsNullOrWhiteSpace(enumMember))
+                return e.ToString();
+
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(enumMember.ToLower());
+        }
+    }
+}
