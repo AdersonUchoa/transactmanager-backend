7597002602e6659badbbde0db671a205a6a407e6
Aderson <iamuchoa@gmail.com>
2026-03-11 21:45:05 -0300

    feat: new interface services

---
+++ Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Requests.Categoria;
+using Application.Responses;
+using Application.Responses.Categoria;
+using Domain.Enums;
+
+namespace Application.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<ApiResponse<CategoriaResponse>> AddAsync(CreateCategoriaRequest request);
+        Task<ApiResponse<CategoriaResponse>> UpdateAsync(int id, UpdateCategoriaRequest request);
+        Task<ApiResponse<CategoriaByIdResponse>> GetByIdAsync(int id);
+        Task<ApiResponse<List<CategoriaResponse>>> GetAllAsync(CategoriaFinalidadeEnum? finalidade = null, string? search = null);
+        Task<ApiResponse<bool>> DeleteAsync(int id);
+        Task<ApiResponse<int>> GetCategoriasCountAsync();
+    }
+}
---
+++ Application/Interfaces/IPessoaService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Requests.Pessoa;
+using Application.Responses;
+using Application.Responses.Pessoa;
+
+namespace Application.Interfaces
+{
+    public interface IPessoaService
+    {
+        Task<ApiResponse<PessoaResponse>> AddAsync(CreatePessoaRequest request);
+        Task<ApiResponse<PessoaResponse>> UpdateAsync(int id, UpdatePessoaRequest request);
+        Task<ApiResponse<PessoaByIdResponse>> GetByIdAsync(int id);
+        Task<ApiResponse<List<PessoaResponse>>> GetAllAsync(string? search = null);
+        Task<ApiResponse<bool>> DeleteAsync(int id);
+        Task<ApiResponse<int>> GetPessoasCountAsync();
+    }
+}
